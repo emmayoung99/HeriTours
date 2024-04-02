ff67777a1fa07a23062331a746de96c83d8f08a9
emmayoung99 <emmapaw24@gmail.com>
2024-04-02 16:01:41 -0400

    Changed ClearMap function and changed button appearance while selected.

--- js/Buttons.js
+++ js/Buttons.js
@@ -1,9 +1,9 @@
 ﻿/* 
 Version         Date            Coder           Comments
 1.0.0           2024-04-02      EYOUNG          Initial   
+*/
 
 
-function toggleSelected(buttonId) {
-    var button = document.getElementById(buttonId);
-    button.classList.toggle("selected");
-}
+
+
+
